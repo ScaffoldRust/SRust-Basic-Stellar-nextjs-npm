b3969584db2d906fb6ec89f1b88879a07e6daef6
jahrulezfrancis <longsaarmuknaan@gmail.com>
2025-03-12 16:33:19 +0100

    fix:reduced code duplication and updated mobile menu background color

--- packages/nextjs/components/navbar.tsx
+++ packages/nextjs/components/navbar.tsx
@@ -2,11 +2,17 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import { theme } from "../config/theme";
-import { Coins, Globe, Menu, X } from "lucide-react";
+import { Globe, Menu, X } from "lucide-react";
+import ConnectWalletButton from "./ui/connectWalletButton";
 
 export const NavBar = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [isScrolled, setIsScrolled] = useState(false);
+
+    const navlinks = [
+        { id: 1, title: "Home", path: "/" },
+        { id: 2, title: "Debug Contracts", path: "#" },
+    ]
 
     useEffect(() => {
         const handleScroll = () => {
@@ -24,8 +30,8 @@ export const NavBar = () => {
     return (
         <nav
             className={`sticky top-0 z-30 flex items-center justify-between px-4 py-3 transition-all duration-300 ${isScrolled
-                    ? "bg-neutral bg-opacity-90 backdrop-blur-sm shadow-lg"
-                    : "bg-transparent"
+                ? "bg-neutral bg-opacity-90 backdrop-blur-sm shadow-lg"
+                : "bg-transparent"
                 }`}
         >
             <div className="flex items-center gap-4">
@@ -46,8 +52,8 @@ export const NavBar = () => {
                 <button
                     onClick={() => setIsOpen(!isOpen)}
                     className={`p-2 rounded-full ${isScrolled
-                            ? "text-black hover:bg-neutral-focus"
-                            : "text-primary hover:bg-primary hover:bg-opacity-10"
+                        ? "text-black hover:bg-neutral-focus"
+                        : "text-primary hover:bg-primary hover:bg-opacity-10"
                         } focus:outline-none transition-colors`}
                 >
                     {isOpen ? <X size={24} /> : <Menu size={24} />}
@@ -56,60 +62,35 @@ export const NavBar = () => {
 
             {/* Desktop menu */}
             <div className="hidden md:flex md:items-center md:gap-8">
-                <Link
-                    href="/"
-                    className={`font-medium ${isScrolled ? "text-black hover:text-primary" : "text-gray-800 hover:text-primary"
-                        }`}
-                >
-                    Home
-                </Link>
-                <Link
-                    href="#"
-                    className={`font-medium ${isScrolled ? "text-black hover:text-primary" : "text-gray-800 hover:text-primary"
-                        }`}
-                >
-                    Debug Contracts
-                </Link>
-                <Link
-                    href="#"
-                    className={`font-medium ${isScrolled ? "text-black hover:text-primary" : "text-gray-800 hover:text-primary"
-                        }`}
-                >
-                    Docs
-                </Link>
-                <button className="ml-4 p-2 btn btn-primary px-6 font-bold shadow-md shadow-primary/30 border-0 rounded-lg hover:shadow-lg hover:shadow-primary/40 hover:brightness-110 hover:cursor-pointer transition-all">
-                    Connect Wallet
-                </button>
-
+                {navlinks.map((navlink) => (
+                    <Link
+                        key={navlink.id}
+                        href={navlink.path}
+                        className={`font-medium ${isScrolled ? "text-black hover:text-primary" : "text-gray-800 hover:text-primary"
+                            }`}
+                    >
+                        {navlink.title}
+                    </Link>
+                ))
+                }
+                <ConnectWalletButton />
             </div>
 
             {/* Mobile menu */}
             {isOpen && (
-                <div className="absolute top-full left-0 right-0 bg-neutral p-4 shadow-xl md:hidden flex flex-col gap-4 border-t border-gray-700">
-                    <Link
-                        href="/"
-                        className="text-white hover:text-primary py-2 font-medium"
-                        onClick={() => setIsOpen(false)}
-                    >
-                        Home
-                    </Link>
-                    <Link
-                        href="/debug"
-                        className="text-white hover:text-primary py-2 font-medium"
-                        onClick={() => setIsOpen(false)}
-                    >
-                        Debug Contracts
-                    </Link>
-                    <Link
-                        href="#"
-                        className="text-white hover:text-primary py-2 font-medium"
-                        onClick={() => setIsOpen(false)}
-                    >
-                        Docs
-                    </Link>
-                    <button className="btn btn-primary w-full font-bold shadow-md shadow-primary/30 border-0 hover:shadow-lg hover:shadow-primary/40 hover:brightness-110 transition-all">
-                        Connect Wallet
-                    </button>
+                <div className="absolute bg-white top-full left-0 right-0 p-4 shadow-xl md:hidden flex flex-col gap-4 border-t border-gray-700">
+                    {navlinks.map((navlink) => (
+                        <Link
+                            key={navlink.id}
+                            href={navlink.path}
+                            className={`font-medium ${isScrolled ? "text-black hover:text-primary" : "text-gray-800 hover:text-primary"
+                                }`}
+                        >
+                            {navlink.title}
+                        </Link>
+                    ))
+                    }
+                    <ConnectWalletButton />
                 </div>
             )}
         </nav>
