6902cf382a8300d791f83698901970e279abad73
MPSxDev <mparedes.digital@gmail.com>
2025-03-30 12:27:35 -0300

    feat: add debug components exports

---
+++ packages/nextjs/components/debug/index.ts
@@ -0,0 +1,5 @@
+export { InvokeContract } from './InvokeContract';
+export { ContractInteraction } from './ContractInteraction';
+export { ContractEvents } from './ContractEvents';
+export { EventItem } from './EventItem';
+export { ContractFunction } from './ContractFunction'; 
